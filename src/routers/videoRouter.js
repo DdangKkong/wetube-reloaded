98a79aab9fe66aa8e4aac629679c0b892e2eb8d4
DdangKkong <dnjsdn8@naver.com>
2023-05-30 23:15:07 +0900

    220530 파라미터

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { watch, edit} from "../controllers/videoController";
+import { watch, edit, upload, see} from "../controllers/videoController";
 
 
 const videoRouter = express.Router();
 
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/upload", upload)
+videoRouter.get("/:id(//d+)", see);
+videoRouter.get("/:id(//d+)/edit", edit);
+videoRouter.get("/:id(//d+)/delete");
 
 export default videoRouter;
 
