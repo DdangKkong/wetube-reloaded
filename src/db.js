67d1ccf9c9ac102b7ceb3cc468329a7640caae6a
DdangKkong <dnjsdn8@naver.com>
2023-06-15 22:38:42 +0900

    230615 github login

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.1/wetube");
+mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -17,7 +18,11 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true}));
 
-app.use(session({secret: "Hello!", resave: true, saveUninitialized: true,}));
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false, 
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL })}));
 //Router 전에 써야한대
 
 
